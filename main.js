3cc1d87295fcea6fa666c4595ff0ba5c254a83f7
athdot <athdot@gmail.com>
2017-04-12 12:00:01 -0600

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.getElementById("head").appendChild(title);
 //iframe
 var ifrm = document.createElement("iframe");
 ifrm.style = "width:100%; height: 100vh; position:absolute; top:0; left: 0;";
-ifrm.src = "https:/\/\athdot.github.io/\home/\";
+ifrm.src = "https:/\/\athdot.github.io/\home/\"
 document.getElementById("body").appendChild(ifrm);
 //iframe end
 //endbody
